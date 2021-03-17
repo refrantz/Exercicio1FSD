dffcb24ad361fdd1812f1bce2974a66d0e79eb09
Alexeiabianna <alexeiabianna@gmail.com>
2021-03-17 22:49:17 +0000

    Insert getters missing

--- Exercicio1FSD/src/Veiculo.java
+++ Exercicio1FSD/src/Veiculo.java
@@ -5,7 +5,6 @@ public class Veiculo {
     private int ano;
     private double valor;
 
-
     public Veiculo(String placa, String marca, String modelo, int ano, double valor) {
         this.placa = placa;
         this.marca = marca;
@@ -14,11 +13,11 @@ public class Veiculo {
         this.valor = valor;
     }
 
-    public String getMarca(){
+    public String getMarca() {
         return marca;
     }
 
-    public int getAno(){
+    public int getAno() {
         return ano;
     }
 
@@ -26,5 +25,29 @@ public class Veiculo {
     public String toString() {
         return "\nVeiculo [ano=" + ano + ", marca=" + marca + ", modelo=" + modelo + ", placa=" + placa + ", valor="
                 + valor + "]";
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
     }
 }
