115331b4ebcd4dd3ac6a24ae7149ec32242ad5b7
DeltaRig <hitomikaleiko@gmail.com>
2021-03-17 09:53:53 -0300

    fix veiculos

--- Exercicio1FSD/src/Veiculo.java
+++ Exercicio1FSD/src/Veiculo.java
@@ -1,7 +1,16 @@
-public interface Veiculo {
-    public String placa;
-    public String marca;
-    public String modelo;
-    public int ano;
-    public double valor;
+public class Veiculo {
+    private String placa;
+    private String marca;
+    private String modelo;
+    private int ano;
+    private double valor;
+
+
+    public Veiculo(String placa, String marca, String modelo, int ano, double valor) {
+        this.placa = placa;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+        this.valor = valor;
+    }
 }
--- Exercicio1FSD/src/VeiculoPassageiros.java
+++ Exercicio1FSD/src/VeiculoPassageiros.java
@@ -1,3 +1,10 @@
-public class VeiculoPassageiros implements Veiculo{
+
+public class VeiculoPassageiros extends Veiculo{
     private int nroPass;
+
+    public VeiculoPassageiros(String placa, String marca, String modelo, int ano, double valor, int nroPass) {
+        super(placa, marca, modelo, ano, valor);
+        //TODO Auto-generated constructor stub
+        this.nroPass = nroPass;
+    }
 }
