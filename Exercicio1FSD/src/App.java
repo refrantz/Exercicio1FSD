60dc36b587487408e140126f7e8c70a9b54d928f
renan <renan262001@gmail.com>
2021-03-17 14:21:53 -0300

    Teste do metodo

--- Exercicio1FSD/src/App.java
+++ Exercicio1FSD/src/App.java
@@ -8,10 +8,15 @@ public class App {
         // Feito para confirmar se o método funciona
         CatalogoVeiculos catalogo = new CatalogoVeiculos();
         List<Veiculo> testMarca = catalogo.consultaPorMarca("EFFA");
+        // Teste do método consultaPorAno tambem
+        List<Veiculo> testAno = catalogo.consultaPorAno(2020);
 
         for(int i=0;i<testMarca.size();i++){
-            System.out.println(testMarca.get(i));
-        } */
+            System.out.println("Marca EFFA: " + testMarca.get(i));
+            System.out.println("Ano 2020: " + testAno.get(i));
+        } 
+        */
+
 
     }
 }
