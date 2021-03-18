d23bf8a7c306762dd0c387d896dc521b03aa9ecd
DeltaRig <hitomikaleiko@gmail.com>
2021-03-18 19:37:07 -0300

    data for veiculosUtilitarios

--- Exercicio1FSD/src/CatalogoVeiculos.java
+++ Exercicio1FSD/src/CatalogoVeiculos.java
@@ -5,19 +5,31 @@ import java.util.List;
 public class CatalogoVeiculos {
 
     //Lista de veiculos hard coded
+    VeiculoUtilitario vu1 = new VeiculoUtilitario("BRA2E19", "Honda", "Civic", 2017, 107200.00, 5, 2); 
+    VeiculoUtilitario vu2 = new VeiculoUtilitario("BRA3E20", "Honda", "Accord", 2020, 107201.00, 5, 2); 
+    VeiculoUtilitario vu3 = new VeiculoUtilitario("BRA4E20", "Honda", "Fit", 2015, 107201.00, 5, 2); 
+        
+
+
     List<Veiculo> veiculos = Arrays.asList(
-        new Veiculo("BRA2E19", "Honda", "Civic", 2016, 107200.00), 
-        new Veiculo("BRA3E20", "Honda", "Accord", 2016, 107201.00),
-        new Veiculo("BRA3E20", "Honda", "Fit", 2016, 107201.00),
-        new Veiculo("BRA3E20", "Audi", "A3", 2016, 107201.00),
-        new Veiculo("BRA3E20", "Effa", "K01", 2016, 107201.00),
-        new Veiculo("BRA3E20", "Effa", "V25", 2016, 107201.00)
+        vu1, vu2, vu3,
+        new Veiculo("BRA5E20", "Audi", "A3", 2006, 107201.00),
+        new Veiculo("BRA6E20", "Effa", "K01", 2019, 107201.00),
+        new Veiculo("BRA7E20", "Effa", "V25", 2020, 107201.00)
         );
 
 
 
-    public Veiculo consultaPorPlaca(String placa){
-        return null;
+    public List<Veiculo> consultaPorPlaca(String placa){
+        List<Veiculo> result = new ArrayList();
+        placa = placa.toLowerCase();
+
+        for(int i = 0; i < veiculos.size(); i++) {
+            if(placa.equals(veiculos.get(i).getPlaca().toLowerCase())){
+                result.add(veiculos.get(i));
+            }
+        }
+        return result;
     }
 
     public List<Veiculo> consultaPorMarca(String marca){
@@ -34,10 +46,20 @@ public class CatalogoVeiculos {
     }
 
     public List<Veiculo> consultaPorAno(int ano){
-        return null;
+       List<Veiculo> result = new ArrayList();
+
+       for(Veiculo v: veiculos){
+            if (v.getAno() == ano){
+                result.add(v);
+            }
+       }
+
+       return result;
     }
 
+    // opções de tipo: "passageiros", "passeio", utilitario
     public List<Veiculo> consultaPorTipo(String tipo){
+
         return null;
     }
 
