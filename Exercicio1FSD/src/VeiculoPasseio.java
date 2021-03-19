f34607e8cfbabea52944857ac97ecea7b9ea8c7f
DeltaRig <hitomikaleiko@gmail.com>
2021-03-19 18:44:37 -0300

    ajust constructor

--- Exercicio1FSD/src/VeiculoPasseio.java
+++ Exercicio1FSD/src/VeiculoPasseio.java
@@ -2,6 +2,7 @@ public class VeiculoPasseio extends Veiculo {
 
     public VeiculoPasseio(String placa, String marca, String modelo, int ano, double valor, double consumoKmLt){
         super(placa, marca, modelo, ano, valor);
+        this.consumoKmLt = consumoKmLt;
     }
 
     private double consumoKmLt;
--- Exercicio1FSD/src/VeiculoUtilitario.java
+++ Exercicio1FSD/src/VeiculoUtilitario.java
@@ -2,6 +2,8 @@ public class VeiculoUtilitario extends Veiculo {
 
     public VeiculoUtilitario(String placa, String marca, String modelo, int ano, double valor, int capCargaTon, int nroEixos){
         super(placa, marca, modelo, ano, valor);
+        this.capCargaTon = capCargaTon;
+        this.nroEixos = nroEixos;
     }
 
     private int capCargaTon;
