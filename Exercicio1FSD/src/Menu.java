636c9b2f11f6dadbef2f4f7309e880206af743cd
Alexeiabianna <alexeiabianna@gmail.com>
2021-03-20 16:31:07 +0000

    fixing bugs

--- Exercicio1FSD/src/Menu.java
+++ Exercicio1FSD/src/Menu.java
@@ -5,15 +5,15 @@ public class Menu {
     private static String r = "======== Sistema Consulta de Veiculos ========";
     private static String s = "Selecione o tipo de Veiculo para iniciar:";
     private static String t = "1: Veiculo de Passageiros \n2: Veiculo de Passeio \n3: Veiculo Utilitario:";
-    private static String u = "0: Sair do sistema:";
+    private static String u = "4: Sair do sistema:";
     private static String v = "Selecione o modo de pesquisa: \n";
     private static String x = "1: Placa \n2: Marca \n3: Ano \n4: Tipo \n0: Finalizar pesquisa.";
     private static String z = "============================================================================";
-    
+
     private static CatalogoVeiculos catalogo = new CatalogoVeiculos();
 
     public static void printInicio() {
-        System.out.println();        
+        System.out.println();
         System.out.println(r);
         System.out.println();
         System.out.println(s);
@@ -28,38 +28,37 @@ public class Menu {
         System.out.println(x);
         System.out.println();
     }
-    
+
     public static void menuInicio() {
         int opcao;
         Scanner entrada = new Scanner(System.in);
         printInicio();
         do {
             opcao = entrada.nextInt();
-            switch(opcao) {
-                case 1:
-                    System.out.println("Veiculo Passageiros");
-                    menuConsulta(); // Falta separar por tipo a pesquisa, da pra fazer algo menuConsulta(opcao)
-                    break;          
+            switch (opcao) {
+            case 1:
+                System.out.println("Veiculo Passageiros");
+                menuConsulta(); // Falta separar por tipo a pesquisa, da pra fazer algo menuConsulta(opcao)
+                break;
 
-                case 2:
-                    System.out.println("Veiculo Passeio");
-                    menuConsulta();
-                    break;
+            case 2:
+                System.out.println("Veiculo Passeio");
+                menuConsulta();
+                break;
 
-                case 3:
-                    System.out.println("Veiculo Utilitario");
-                    menuConsulta();                    
-                    break;
+            case 3:
+                System.out.println("Veiculo Utilitario");
+                menuConsulta();
+                break;
 
-                case 0:
-                    System.out.println("Encerrando aplicacao");
-                    return;
+            case 4:
+                System.out.println("Encerrando Aplicacao");
 
-                default:
-                    System.out.println("Opcao Invalida");
+            default:
+                System.out.println("Opcao Invalida");
             }
 
-        }while(opcao != 0);
+        } while (opcao != 4);
     }
 
     public static void menuConsulta() {
@@ -69,45 +68,45 @@ public class Menu {
         printConsulta();
         do {
             opcao = entrada.nextInt();
-            switch(opcao) {
-                case 1:
-                    System.out.println("Digite a placa: ");
-                    consulta = entrada.next();                    
-                    retornaPlaca(consulta);
-                    System.out.println(z);
-                    break;
+            switch (opcao) {
+            case 1:
+                System.out.println("Digite a placa: ");
+                consulta = entrada.next();
+                retornaPlaca(consulta);
+                System.out.println(z);
+                break;
 
-                case 2:
-                    System.out.println("Digite a marca: ");
-                    consulta = entrada.next();                    
-                    retornaMarca(consulta);
-                    System.out.println(z);
-                    break;
+            case 2:
+                System.out.println("Digite a marca: ");
+                consulta = entrada.next();
+                retornaMarca(consulta);
+                System.out.println(z);
+                break;
 
-                case 3:
-                    System.out.println("Digite o ano: ");
-                    consulta = entrada.next();                    
-                    retornaAno(Integer.parseInt(consulta));
-                    System.out.println(z);
-                    break;
-                
-                case 4:
-                    System.out.println("Digite o tipo: ");
-                    consulta = entrada.next();
-                    retornaTipo(consulta);
-                    System.out.println(z);
-                    break;
+            case 3:
+                System.out.println("Digite o ano: ");
+                consulta = entrada.next();
+                retornaAno(Integer.parseInt(consulta));
+                System.out.println(z);
+                break;
 
-                case 0:
-                    System.out.println("Fim da pesquisa \nVoltando ao menu anterior");
-                    System.out.println(z);
-                    menuInicio();
-                    break;
+            case 4:
+                System.out.println("Digite o tipo: ");
+                consulta = entrada.next();
+                retornaTipo(consulta);
+                System.out.println(z);
+                break;
 
-                default:
-                    System.out.println("Opcao Invalida");
+            case 0:
+                System.out.println("Fim da pesquisa \nVoltando ao menu anterior");
+                System.out.println(z);
+                menuInicio();
+                break;
+
+            default:
+                System.out.println("Opcao Invalida");
             }
-        }while(opcao != 0);
+        } while (opcao != 0);
     }
 
     public static void retornaPlaca(String placa) {
@@ -125,5 +124,5 @@ public class Menu {
     public static void retornaTipo(String tipo) {
         System.out.println(catalogo.consultaPorTipo(tipo));
     }
-    
+
 }
