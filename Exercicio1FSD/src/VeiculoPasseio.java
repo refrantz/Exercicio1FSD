b216dabb27553626f0dc4a2797252cecadae1a9b
Alexeiabianna <alexeiabianna@gmail.com>
2021-03-20 00:49:19 +0000

    ToString is done

--- Exercicio1FSD/src/VeiculoPasseio.java
+++ Exercicio1FSD/src/VeiculoPasseio.java
@@ -1,8 +1,17 @@
 public class VeiculoPasseio extends Veiculo {
 
+    private double consumoKmLt;
+
     public VeiculoPasseio(String placa, String marca, String modelo, int ano, double valor){
         super(placa, marca, modelo, ano, valor);
-    }
+    }    
 
-    private double consumoKmLt;
+    public String toString() {
+        return "\nVeiculo [ano=" + getAno() 
+                + ", marca=" + getMarca() 
+                + ", modelo="+ getModelo() 
+                + ", placa=" + getPlaca()
+                + ", valor=" + getValor()
+                + ", consumo Km/L=" + consumoKmLt +"]";
+    }
 }
--- Exercicio1FSD/src/VeiculoUtilitario.java
+++ Exercicio1FSD/src/VeiculoUtilitario.java
@@ -1,9 +1,19 @@
 public class VeiculoUtilitario extends Veiculo {
+    
+    private int capCargaTon;
+    private int nroEixos;
 
     public VeiculoUtilitario(String placa, String marca, String modelo, int ano, double valor){
         super(placa, marca, modelo, ano, valor);
     }
 
-    private int capCargaTon;
-    private int nroEixos;
+    public String toString() {
+        return "\nVeiculo [ano=" + getAno() 
+                + ", marca=" + getMarca() 
+                + ", modelo="+ getModelo() 
+                + ", placa=" + getPlaca()
+                + ", valor=" + getValor()
+                + ", capacidade de carga=" + capCargaTon 
+                + ", numero de eixos=" + nroEixos +"]";
+    }
 }
