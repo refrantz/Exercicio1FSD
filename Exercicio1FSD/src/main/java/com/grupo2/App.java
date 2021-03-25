96d784ce38a57d32dac514ccafc741ba59d39572
renan <renan262001@gmail.com>
2021-03-25 15:09:01 -0300

    leitor csv para veiculo passeio (a fazer: converter alguns valores para inteiro e double)

--- Exercicio1FSD/src/main/java/com/grupo2/App.java
+++ Exercicio1FSD/src/main/java/com/grupo2/App.java
@@ -11,7 +11,7 @@ import org.apache.commons.csv.CSVRecord;
 public class App {
     public static void main(String[] args) throws Exception {
         try ( 
-            Reader reader = Files.newBufferedReader(Paths.get("D:/PUCRS/3_SEMESTRE/Fundamentos_de_D.de_Software/Exercicio1Maven/consultaveiculos/src/main/java/com/grupo2/Entradas/veiculoPassageiros.csv"));
+            Reader reader = Files.newBufferedReader(Paths.get("D:/PUCRS/3_SEMESTRE/Fundamentos_de_D.de_Software/Exercicio1Maven/consultaveiculos/src/main/java/com/grupo2/Entradas/veiculoPasseio.csv"));
             CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT);
         ) { 
             for (CSVRecord csvRecord : csvParser) { 
@@ -19,16 +19,12 @@ public class App {
                 String placa = csvRecord.get(0);
                 String marca = csvRecord.get(1); 
                 String modelo = csvRecord.get(2);
-                int ano = Integer.parseInt(csvRecord.get(3));
-                double valor = Double.parseDouble(csvRecord.get(4)); 
-                System.out.println("Record No - " + csvRecord.getRecordNumber()); 
-                System.out.println("---------------"); 
-                System.out.println("Placa : " + placa); 
-                System.out.println("Marca : " + marca); 
-                System.out.println("Modelo : " + modelo);
-                System.out.println("Modelo : " + ano); 
-                System.out.println("Preco : " + valor); 
-                System.out.println("---------------\n\n"); 
+                String ano = csvRecord.get(3);
+                String valor = csvRecord.get(4);
+                String consumoKmLt = csvRecord.get(5);
+                //todo converter valores de ano para inteiro, valor para double e consumoKmLt para inteiro
+
+                //new VeiculoPassageiros(placa, marca, modelo, ano, valor, consumoKmLt);
             } 
     }
         Menu user = new Menu();
