ea5d6e3679d76153aed39ab6315e16274bc0dec2
renan <renan262001@gmail.com>
2021-03-26 15:24:56 -0300

    Leitura e instanciacao de todos tipos de veiculos

--- Exercicio1FSD/src/main/java/com/grupo2/App.java
+++ Exercicio1FSD/src/main/java/com/grupo2/App.java
@@ -1,7 +1,6 @@
 package com.grupo2;
 
 import java.io.BufferedReader;
-import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -14,23 +13,48 @@ import org.apache.commons.csv.CSVRecord;
 public class App {
     public static void main(String[] args) throws Exception {
         try ( 
-            BufferedReader reader = Files.newBufferedReader(Paths.get(System.getProperty("user.dir") + "/src/main/java/com/grupo2/Entradas/veiculoPasseio.csv"));
+            BufferedReader reader = Files.newBufferedReader(Paths.get(System.getProperty("user.dir") + "/src/main/java/com/grupo2/Entradas/veiculoPassageiros.csv"));
+            BufferedReader reader2 = Files.newBufferedReader(Paths.get(System.getProperty("user.dir") + "/src/main/java/com/grupo2/Entradas/veiculoPasseio.csv"));
+            BufferedReader reader3 = Files.newBufferedReader(Paths.get(System.getProperty("user.dir") + "/src/main/java/com/grupo2/Entradas/veiculoUtilitario.csv"));
             CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT);
+            CSVParser csvParser2 = new CSVParser(reader2, CSVFormat.DEFAULT);
+            CSVParser csvParser3 = new CSVParser(reader3, CSVFormat.DEFAULT);
         ) { 
             reader.readLine();
+            reader2.readLine();
+            reader3.readLine();
             List<Veiculo> veiculos = new ArrayList<Veiculo>();
             for (CSVRecord csvRecord : csvParser) { 
-                // Accessing Values by Column Index 
+                String placa = csvRecord.get(0);
+                String marca = csvRecord.get(1); 
+                String modelo = csvRecord.get(2);
+                Integer ano = Integer.parseInt(csvRecord.get(3));
+                Double valor = Double.parseDouble(csvRecord.get(4));
+                Integer nroPass = Integer.parseInt(csvRecord.get(5));
+
+                veiculos.add(new VeiculoPassageiros(placa, marca, modelo, ano, valor, nroPass));
+            } 
+            for (CSVRecord csvRecord : csvParser2) { 
                 String placa = csvRecord.get(0);
                 String marca = csvRecord.get(1); 
                 String modelo = csvRecord.get(2);
                 Integer ano = Integer.parseInt(csvRecord.get(3));
                 Double valor = Double.parseDouble(csvRecord.get(4));
                 Integer consumoKmLt = Integer.parseInt(csvRecord.get(5));
-                //todo converter valores de ano para inteiro, valor para double e consumoKmLt para inteiro
 
-                veiculos.add(new VeiculoPassageiros(placa, marca, modelo, ano, valor, consumoKmLt));
-            } 
+                veiculos.add(new VeiculoPasseio(placa, marca, modelo, ano, valor, consumoKmLt));
+            }
+            for (CSVRecord csvRecord : csvParser3) { 
+                String placa = csvRecord.get(0);
+                String marca = csvRecord.get(1); 
+                String modelo = csvRecord.get(2);
+                Integer ano = Integer.parseInt(csvRecord.get(3));
+                Double valor = Double.parseDouble(csvRecord.get(4));
+                Integer capCargaTon = Integer.parseInt(csvRecord.get(5));
+                Integer nroEixos = Integer.parseInt(csvRecord.get(6));
+
+                veiculos.add(new VeiculoUtilitario(placa, marca, modelo, ano, valor, capCargaTon, nroEixos));
+            }
             System.out.println(veiculos);
     }
         Menu user = new Menu();
