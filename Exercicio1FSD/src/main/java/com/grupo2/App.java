5ce228e931e1cec001b5c781c62139f1b3c0628c
renan <renan262001@gmail.com>
2021-03-26 15:13:51 -0300

    leitura e construcao da primeira lista de veiculos done

--- Exercicio1FSD/src/main/java/com/grupo2/App.java
+++ Exercicio1FSD/src/main/java/com/grupo2/App.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
@@ -16,19 +18,20 @@ public class App {
             CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT);
         ) { 
             reader.readLine();
+            List<Veiculo> veiculos = new ArrayList<Veiculo>();
             for (CSVRecord csvRecord : csvParser) { 
                 // Accessing Values by Column Index 
                 String placa = csvRecord.get(0);
                 String marca = csvRecord.get(1); 
                 String modelo = csvRecord.get(2);
-                String ano = csvRecord.get(3);
-                String valor = csvRecord.get(4);
-                String consumoKmLt = csvRecord.get(5);
-                Integer teste = Integer.parseInt(ano);
+                Integer ano = Integer.parseInt(csvRecord.get(3));
+                Double valor = Double.parseDouble(csvRecord.get(4));
+                Integer consumoKmLt = Integer.parseInt(csvRecord.get(5));
                 //todo converter valores de ano para inteiro, valor para double e consumoKmLt para inteiro
 
-                //new VeiculoPassageiros(placa, marca, modelo, ano, valor, consumoKmLt);
+                veiculos.add(new VeiculoPassageiros(placa, marca, modelo, ano, valor, consumoKmLt));
             } 
+            System.out.println(veiculos);
     }
         Menu user = new Menu();
         user.menuInicio();
